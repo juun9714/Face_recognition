0bc34239a883331660ffb20b04f4708c74d464b3
권준희 <june@gwonjunhuiui-MacBookAir.local>
2023-02-09 17:55:08 +0900

    demo

---
+++ demo.py
@@ -0,0 +1,51 @@
+import cv2
+import os
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+
+
+for i in range(1,4):
+    vidcap = cv2.VideoCapture('./video_files/episode%d.avi' % i)
+    count = 1 # 추출 이미지 개수 카운트
+    real_count=1
+    os.mkdir("./image_files/episode%d"%i)  # 폴더 생성
+    while(vidcap.isOpened()):
+        if real_count>100:
+            vidcap.release()
+            break
+        # 영상이 지속되는 동안 반복
+        ret, image = vidcap.read()
+        image = cv2.resize(image, (960, 540)) # 이미지 사이즈 960x540으로 변경
+        
+        if(int(vidcap.get(1)) % 30 == 0): 
+            # vidcap.get(1) -> 현재 프레임 숫자 반환, 프레임 숫자를 15로 나눈 나머지가 0이면 (1초당 2장, 만약 숫자가 15가 아니고 30이면 1초당 1장)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            flag=0
+
+            for (x,y,w,h) in faces:
+                # cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = image[y:y+h, x:x+w]
+                eyes = eye_cascade.detectMultiScale(roi_gray)
+                
+                for (ex,ey,ew,eh) in eyes:
+                    flag=1
+                    # cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+
+                if flag==1:
+                    if count%15==0:
+                        cropped=image[y :y + h , x :x + w]
+                        # cv2.imwrite("./night/100/epi4_100_face/cropped_%d.png"%(i/18), cropped)
+                        cv2.imwrite("./image_files/episode{}/cropped_{}.jpg".format(i, real_count),cropped) # 추출된 이미지가 저장되는 경로와 파일명을 지정.
+                        real_count+=1
+                        print("Saved %d'th face cropped image"%real_count)
+                    count += 1 # 저장한 이미지 개수 카운트
+                    flag=0
+            
+    vidcap.release() # close video file
+
+# frame extract done, face extract starts
